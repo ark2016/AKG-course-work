140fb98056546bb3a4ae25240c6b51ea509d3b7a
Ark <A6611688@yandex.ru>
2024-11-10 20:10:28 +0300

    add: parser for 500px

--- app/routers/questions.py
+++ app/routers/questions.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from ..utils.parser.parser import download_images  # Импортируем функцию
-from ..schemas.questions import DownloadImagesRequest  # Импортируем схему
+from ..utils.parser.parser500px import scrape_500px_images  # Импортируем функцию
+from ..schemas.questions import DownloadImagesRequest, Scrape500pxImagesRequest  # Импортируем схемы
 
 router = APIRouter(
     tags=["Questions"]
@@ -18,6 +19,17 @@ def api_download_images(request: DownloadImagesRequest):
         print(f"Received request: URL={request.url}, Folder={request.folder}")
         download_images(url=request.url, folder=request.folder)
         return {"message": f"Images downloaded successfully to {request.folder}"}
+    except Exception as e:
+        print(f'Ошибка при обработке запроса: {e}')
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Маршрут для скрапинга изображений с 500px
+@router.post("/scrape_500px_images")
+def api_scrape_500px_images(request: Scrape500pxImagesRequest):
+    try:
+        print(f"Received request: URL={request.url},  Folder={request.folder}")
+        scrape_500px_images(url=request.url, folder=request.folder)
+        return {"message": f"Images scraped successfully to {request.folder}"}
     except Exception as e:
         print(f'Ошибка при обработке запроса: {e}')
         raise HTTPException(status_code=500, detail=str(e))
--- app/schemas/questions.py
+++ app/schemas/questions.py
@@ -4,5 +4,11 @@ from typing import Optional
 
 # Модель запроса для download_images
 class DownloadImagesRequest(BaseModel):
-    url: str
+    url: str = "https://commons.wikimedia.org/w/index.php?search=red+fox&title=Special:MediaSearch&go=Go&type=image"
+    folder: Optional[str] = "test"
+
+
+# Модель запроса для scrape_500px_images
+class Scrape500pxImagesRequest(BaseModel):
+    url: str = "https://500px.com/search?q=red%20fox&type=photos&sort=relevance"
     folder: Optional[str] = "test"
